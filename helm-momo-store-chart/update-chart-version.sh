b6dcc5e58eda2f8ba3fe84d27186ede1fcbf6c7b
Valery <valery@Valery.VB>
2024-01-10 13:44:29 +0300

    Создал манифесты для запуска argo в кластере

--- helm-momo-store-chart/update-chart-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HELM_CHART_VERSION:"0.1.$CI_PIPELINE_ID"
-
-echo "Current version: $HELM_CHART_VERSION"
-sed -i "s/^version:.*/version: $HELM_CHART_VERSION/" Chart.yaml
-helm package . -d ./helm-releases
